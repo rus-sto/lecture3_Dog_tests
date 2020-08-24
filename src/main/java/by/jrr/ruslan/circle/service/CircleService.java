6fba841954f961ee967813c02583f9a0c3b3dcc7
rus-sto <rus-sto@tut.by>
2020-08-24 13:30:57 +0300

    add source folder to git

---
+++ src/main/java/by/jrr/ruslan/circle/service/CircleService.java
@@ -0,0 +1,11 @@
+package by.jrr.ruslan.circle.service;
+
+import by.jrr.ruslan.circle.bean.Circle;
+
+public class CircleService {
+
+    public  double calculateArea(Circle circle) {
+        return circle.getRadius()*circle.getRadius()*3.14;
+    }
+
+}
