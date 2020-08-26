de6d1e33d95e00210daaf33296fc11830ee4bb98
rus-sto <rus-sto@tut.by>
2020-08-26 17:13:47 +0300

    add circle folder to git

--- src/main/java/by/jrr/ruslan/circle/App.java
+++ src/main/java/by/jrr/ruslan/circle/App.java
@@ -3,5 +3,6 @@ package by.jrr.ruslan.circle;
 public class App {
     public static void main(String[] args) {
 //        the entry point to be implemented
+
     }
 }
--- src/main/java/by/jrr/ruslan/circle/service/CircleService.java
+++ src/main/java/by/jrr/ruslan/circle/service/CircleService.java
@@ -4,6 +4,7 @@ import by.jrr.ruslan.circle.bean.Circle;
 
 public class CircleService {
 
+
     public  double calculateArea(Circle circle) {
         return circle.getRadius()*circle.getRadius()*3.14;
     }
